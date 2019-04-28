7559a3938be582177774f0f5c7e7e5c91c41b2af
rahulshenoykarkala <rahulshenoykarkala@gmail.com>
2019-04-28 13:25:03 +0530

    Flow creation (+jquery)

--- player.js
+++ player.js
@@ -1,1 +1,26 @@
 //ADD YOUR CODE HERE.
+getItemsById = (list, id) => list.filter(item => item.id === id)
+getFirst = (list) => list.length > 0 ? list[0] : null;
+
+class Player{
+    constructor(data){
+        this.steps = data.steps;
+        this.step = this.steps[0];
+    }
+    play(){
+        const element = $(this.step.selector)[0];
+        element.style.backgroundColor = "#999";
+        const interaction = (event) => {
+            element.removeEventListener("click", interaction);
+            element.style.backgroundColor = "#fff";
+            this.playNext();
+        };
+        element.addEventListener("click", interaction);
+    }
+    playNext(){
+        this.step = getFirst(getItemsById(this.steps, this.step.next));
+        this.play();
+    }
+}
+
+new Player(getGuideData()).play();
